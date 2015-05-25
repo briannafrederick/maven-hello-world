f51facbd415e7f6501fa4f6d9e40cba4cd875450
Brianna Noland <Brianna.noland@gmail.com>
2015-05-24 23:00:01 -0700

    Adding dependencies

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,17 @@
 package com.mycompany.app;
 
+import org.apache.log4j.Logger;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    private static final Logger LOG = Logger.getLogger(App.class);
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        LOG.info( "Hello World!" );
     }
 }
